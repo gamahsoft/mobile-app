180cf3c21c483630520aec6679e00ea68500014b
Rami <ramyk@hotmail.com>
2025-02-15 19:20:45 -0500

    mobile application

---
+++ frontend/screens/Home.jsx
@@ -0,0 +1,22 @@
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useEffect } from 'react';
+
+
+const Home = ({ navigation }) => {
+  
+  return (
+    <>
+      
+      <View>
+        
+        <View ext1='Available' text2='Services'>
+          
+        </View>
+     
+      </View>
+      
+    </>
+  );
+};
+
+export default Home;
