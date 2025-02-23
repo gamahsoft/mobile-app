c0a1044c508f55c92fd6809dd1230b2548fafc6a
Rami <ramyk@hotmail.com>
2025-02-23 14:11:52 -0500

    database drizzle setup with products table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
-import express, {Router} from "express";
-import productsRouter from './routes/products/index'
+import express, { json, urlencoded } from "express";
+import productsRouter from "./routes/products/index";
 
+//order defined matters. these are middleware functions to encode data in the url body
 const app = express();
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use('/products', productsRouter)
+app.use("/products", productsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
