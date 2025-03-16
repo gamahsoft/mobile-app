96cf34fb84ef279a8fdde0695dfb60107a424e9b
Rami <ramyk@hotmail.com>
2025-03-16 18:21:20 -0400

    authentication

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -7,40 +7,95 @@ import { VStack } from "@/components/ui/vstack";
 import { EyeIcon, EyeOffIcon } from "@/components/ui/icon";
 import { useState } from "react";
 import { HStack } from "@/components/ui/hstack";
+import { useMutation } from "@tanstack/react-query";
+import { login, signup } from "@/api/auth";
+import { useAuth } from "@/store/authStore";
+import { Redirect } from "expo-router";
 // import React from "react";
 
 export default function LoginScreen() {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const setUser = useAuth((s) => s.setUser);
+  const setToken = useAuth((s) => s.setToken);
+  const isLoggedIn = useAuth((s) => !!s.token);
+
+  const loginMutation = useMutation({
+    mutationFn: () => login(email, password),
+    onSuccess: (data) => {
+      console.log("SUccess: ", data);
+      if (data.user && data.token) {
+        setUser(data.user);
+        setToken(data.token);
+      }
+    },
+    onError: () => {
+      console.log("Error");
+    },
+  });
+
+  const signupMutation = useMutation({
+    mutationFn: () => signup(email, password),
+    onSuccess: (data) => {
+      console.log("SUccess sign up: ", data);
+      if (data.user && data.token) {
+        setUser(data.user);
+        setToken(data.token);
+      }
+    },
+    onError: (error) => {
+      console.log("Error: ", error);
+    },
+  });
+
   const handleState = () => {
     setShowPassword((showState) => {
       return !showState;
     });
   };
+
+  if (isLoggedIn) {
+    return <Redirect href={"/"} />;
+  }
+
   return (
     // <FormControl className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2">
-    <FormControl className="p-4 border rounded-lg border-outline-300 bg-white m-2">
+    <FormControl
+      isInvalid={loginMutation.error || signupMutation.error}
+      className="p-4 border rounded-lg border-outline-300 bg-white m-2"
+    >
       <VStack space="xl">
         <Heading className="text-typography-900 pt-3">Login</Heading>
         <VStack space="xs">
           <Text className="text-typography-500">Email</Text>
           <Input className="min-w-[250px]">
-            <InputField type="text" />
+            <InputField value={email} onChangeText={setEmail} type="text" />
           </Input>
         </VStack>
         <VStack space="xs">
           <Text className="text-typography-500">Password</Text>
           <Input className="text-center">
-            <InputField type={showPassword ? "text" : "password"} />
+            <InputField
+              value={password}
+              onChangeText={setPassword}
+              type={showPassword ? "text" : "password"}
+            />
             <InputSlot className="pr-3" onPress={handleState}>
               <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
             </InputSlot>
           </Input>
         </VStack>
         <HStack space="sm">
-          <Button className="flex-1" variant="outline" onPress={() => {}}>
+          <Button
+            className="flex-1"
+            variant="outline"
+            onPress={() => signupMutation.mutate()}
+          >
             <ButtonText>Sign up</ButtonText>
           </Button>
-          <Button className="flex-1" onPress={() => {}}>
+          <Button className="flex-1" onPress={() => loginMutation.mutate()}>
             <ButtonText>Sign in</ButtonText>
           </Button>
         </HStack>
